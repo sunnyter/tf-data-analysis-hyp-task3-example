46b127c5dc3d9a56c9904bf284b15ee599ee79de
sunnyter <129841599+sunnyter@users.noreply.github.com>
2023-04-13 17:43:13 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def solution(x_cont: np.array, x_test: np.array) -> bool: # Одна или дв
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    d = stats.ttest_ind(a=x_cont, b = x_test, random_state=2, alternative='less')
+    d = stats.cramervonmises_2samp(x_cont, x_test)
 
     return  d.pvalue < 0.07 # Ваш ответ, True или False
     
